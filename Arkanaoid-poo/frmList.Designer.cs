167e6c2d8120dd5105c8e8e48f2be79ec26f0a24
MJZF00155619 <00155619@uca.edu.sv>
2020-06-05 19:53:35 -0600

    Modify frmlist label

--- Arkanaoid-poo/frmList.Designer.cs
+++ Arkanaoid-poo/frmList.Designer.cs
@@ -69,10 +69,10 @@ namespace Arkanaoid_poo
             // 
             // label2
             // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label2.Font = new System.Drawing.Font("Old English Text MT", 48F, System.Drawing.FontStyle.Regular,
                 System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.label2.Image = ((System.Drawing.Image) (resources.GetObject("label2.Image")));
             this.label2.Location = new System.Drawing.Point(469, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(461, 103);
@@ -82,10 +82,10 @@ namespace Arkanaoid_poo
             // 
             // label1
             // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label1.Font = new System.Drawing.Font("Old English Text MT", 48F, System.Drawing.FontStyle.Regular,
                 System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.label1.Image = ((System.Drawing.Image) (resources.GetObject("label1.Image")));
             this.label1.Location = new System.Drawing.Point(3, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(460, 103);
